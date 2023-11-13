6c92b6bc77c14fe7c7fd9f2a004960846e67c187
Yahya Iddris Fatah <123132207+balatwist@users.noreply.github.com>
2023-11-12 22:03:29 -0600

    update

--- Question2/Stream2.java
+++ Question2/Stream2.java
@@ -1,16 +1,22 @@
 package Question2;
 
-import java.nio.charset.IllegalCharsetNameException;
+
 import java.util.Arrays;
-import java.util.IntSummaryStatistics;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Stream;
 
 public class Stream2 {
     public static void main(String[] args) {
 
-       Stream<Integer> myIntStream= Stream.of(23,12,34,60,2,11,55,30,27);
 
-       
+      Stream<Integer> myIntStream= Stream.of(23,12,34,60,2,11,55,30,27);
+        System.out.println("Maximum :" +myIntStream.max(Integer::compareTo).orElse(null));
+
+
+        myIntStream=Stream.of(23,12,34,60,2,11,55,30,27);
+        System.out.println("Minimum :" +myIntStream.min(Integer::compareTo).orElse(null));
+
+
     }
 }
