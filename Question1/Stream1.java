313c168225012ea9a7d4114d1fd44997fbd2a510
Yahya Iddris Fatah <123132207+balatwist@users.noreply.github.com>
2023-11-12 21:45:43 -0600

    update

---
+++ Question1/Stream1.java
@@ -0,0 +1,14 @@
+package Question1;
+
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class Stream1 {
+    public static void main(String[] args) {
+        Stream<String> stringStream =Stream.of("Bill","Thomas","Mary");
+        System.out.println(stringStream.collect(Collectors.joining(", ")));
+
+    }
+
+
+}
---
+++ Question2/Stream2.java
@@ -0,0 +1,16 @@
+package Question2;
+
+import java.nio.charset.IllegalCharsetNameException;
+import java.util.Arrays;
+import java.util.IntSummaryStatistics;
+import java.util.List;
+import java.util.stream.Stream;
+
+public class Stream2 {
+    public static void main(String[] args) {
+
+       Stream<Integer> myIntStream= Stream.of(23,12,34,60,2,11,55,30,27);
+
+       
+    }
+}
